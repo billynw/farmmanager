03cf6b72605e9c358ce89aeb75afe074a2034750
billynw <billynwc@gmail.com>
2026-04-02 15:46:57 +0900

    Enable SQLAlchemy SQL logging with echo=True

--- crop-api/database.py
+++ crop-api/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from config import settings
 
-engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
